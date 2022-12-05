d44de0b6ed703a9f122ec69076feed6e07cafa4d
Ili <amandine.lieau@gmail.com>
2022-12-05 15:36:43 +0100

    commit_V2_Bienvenue_V1

---
+++ js/index.js
@@ -0,0 +1,24 @@
+/* ---------- Hamburger Animation ---------- */
+
+const hamburgerToggler = document.querySelector(".hamburger");
+const navLinksContainer = document.querySelector(".navlinks-container");
+
+const toggleNav = () => {
+	hamburgerToggler.classList.toggle("open");
+	
+	const ariaToggle = hamburgerToggler.getAttribute("aria-expanded") === "true" ? "false" : "true";
+	hamburgerToggler.setAttribute("aria-expanded", ariaToggle);
+
+	navLinksContainer.classList.toggle("open");
+}
+
+hamburgerToggler.addEventListener('click', toggleNav);
+
+new ResizeObserver(entries => {
+	if(entries[0].contentRect.width <= 700){
+		navLinksContainer.style.transition = "transform 0.3s ease-out";
+	}  else {
+		navLinksContainer.style.transition = "non";
+	}
+}).observe(document.body);
+
